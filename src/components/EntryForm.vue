85502654fcf1c6335d61cc602c5fb261b2cc02c6
tassun <32536248+tassun@users.noreply.github.com>
2024-11-05 15:28:09 +0700

    initial project

---
+++ src/components/EntryForm.vue
@@ -0,0 +1,325 @@
+<template>
+  <DialogForm ref="dialogForm">
+    <template v-slot:header>
+      <h4 class="modal-title" v-if="insertMode">{{ labels.title_new }}</h4>
+      <h4 class="modal-title" v-if="updateMode">{{ labels.title_edit }}</h4>
+    </template>
+    <template v-slot:default>
+        <div class="row row-height">
+          <div class="col-height col-md-5">
+            <label for="branch">{{ labels.branch_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.branch.$error}">
+              <input type="text" ref="branch" v-model="localData.branch" id="branch" class="form-control input-md" maxlength="50" :disabled="disabledKeyField" /> 
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.branch.$error" class="has-error">{{ v$.branch.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-9">
+            <label for="nameen">{{ labels.nameen_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.nameen.$error}">
+              <input type="text" ref="nameen" v-model="localData.nameen" id="nameen" class="form-control input-md" maxlength="100" /> 
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.nameen.$error" class="has-error">{{ v$.nameen.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-9">
+            <label for="nameth">{{ labels.nameth_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.nameth.$error}">
+              <input type="text" ref="nameth" v-model="localData.nameth" id="nameth" class="form-control input-md" maxlength="100" /> 
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.nameth.$error" class="has-error">{{ v$.nameth.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-11">
+            <label for="addressen">{{ labels.addressen_label }}</label>
+            <input type="text" ref="addressen" v-model="localData.addressen" id="addressen" class="form-control input-md" maxlength="200" /> 
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-9">
+            <label for="addressth">{{ labels.addressth_label }}</label>
+            <input type="text" ref="addressth" v-model="localData.addressth" id="addressth" class="form-control input-md" maxlength="200" /> 
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-5">
+            <label for="branchtype">{{ labels.branchtype_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.branchtype.$error}">
+              <select ref="branchtype" id="branchtype" v-model="localData.branchtype" class="form-control input-md">
+                <option v-for="item in dataCategory.tkbranchtype" :key="item.id" :value="item.id">{{item.text}}</option>
+              </select>
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.branchtype.$error" class="has-error">{{ v$.branchtype.$errors[0].$message }}</span>
+          </div>
+        </div>
+    </template>
+    <template v-slot:footer>
+      <button ref="savebutton" id="savebutton" class="btn btn-dark btn-sm" @click="saveClick" v-if="insertMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.save_button }}</button>
+      <button ref="updatebutton" id="updatebutton" class="btn btn-dark btn-sm" @click="updateClick" v-if="updateMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.update_button }}</button>
+      <button class="btn btn-dark btn-sm" data-dismiss="modal"><em class="fa fa-close fa-btn-icon"></em>{{ labels.cancel_button }}</button>
+    </template>
+  </DialogForm>
+</template>
+<script>
+import { ref, computed, watch } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import { required, helpers } from '@vuelidate/validators';
+import $ from "jquery";
+import { DEFAULT_CONTENT_TYPE, getApiUrl, disableControls }  from '@willsofts/will-app';
+import { startWaiting, stopWaiting, submitFailure, detectErrorResponse }  from '@willsofts/will-app';
+import { confirmUpdate, confirmSave, confirmDelete, successbox, serializeParameters } from '@willsofts/will-app';
+import DialogForm from './DialogForm.vue';
+
+const APP_URL = "/api/sfte019";
+const defaultData = {
+  site: "",
+  branch: "",
+  nameen: "",
+  nameth: "",
+  addressen: "",
+  addressth: "",
+  branchtype: "SB",
+};
+
+export default {
+  components: {
+    DialogForm
+  },
+  props: {
+    modes: Object,
+    labels: Object,
+    dataCategory: Object
+  },
+  emits: ["data-saved","data-updated","data-deleted"],
+  setup(props) {
+    const mode = ref({action: "new", ...props.modes});
+    const localData = ref({ ...defaultData }); 
+    const disabledKeyField = ref(false);
+    const reqalert = ref(props.labels.empty_alert);
+    const requiredMessage = () => {
+      return helpers.withMessage(reqalert, required);
+    }
+    const validateRules = computed(() => { 
+      return {
+        branch: { required: requiredMessage() },
+        nameen: { required: requiredMessage() },
+        nameth: { required: requiredMessage() },
+        branchtype: { required: requiredMessage() },
+      } 
+    });
+    const v$ = useVuelidate(validateRules, localData, { $lazy: true, $autoDirty: true });
+    return { mode, v$, localData, disabledKeyField, reqalert };
+  },
+  created() {
+    watch(this.$props, (newProps) => {      
+      this.reqalert = newProps.labels.empty_alert;
+      this.agealert = newProps.labels.age_alert;
+    });
+  },
+  computed: {
+    insertMode() {
+      return this.mode.action=="insert" || this.mode.action=="new";
+    },
+    updateMode() {
+      return this.mode.action=="update" || this.mode.action=="edit";
+    },
+  },
+  mounted() {
+    this.$nextTick(function () {
+      $("#modaldialog_layer").find(".modal-dialog").draggable();
+    });
+  },
+  methods: {
+    reset(newData,newMode) {
+      if(newData) this.localData = {...newData};
+      if(newMode) this.mode = {...newMode};
+    },
+    submit() {      
+      this.$emit('update:formData', this.localData);
+    },
+    async saveClick() {
+      console.log("click: save");
+      disableControls($("#savebutton"));
+      let valid = await this.validateForm();
+      if(!valid) return;
+      this.startSaveRecord();
+    },
+    async updateClick() {
+      console.log("click: update");
+      disableControls($("#updatebutton"));
+      let valid = await this.validateForm();
+      if(!valid) return;
+      this.startUpdateRecord();
+    },
+    async validateForm(focusing=true) {
+      const valid = await this.v$.$validate();
+      console.log("validate form: valid",valid);
+      console.log("error:",this.v$.$errors);
+      if(!valid) {
+        if(focusing) {
+          this.focusFirstError();
+        }
+        return false;
+      }
+      return true;
+    },
+    focusFirstError() {
+      if(this.v$.$errors && this.v$.$errors.length > 0) {
+        let input = this.v$.$errors[0].$property;
+        let el = this.$refs[input];
+        if(el) el.focus(); //if using ref
+        else $("#"+input).trigger("focus"); //if using id
+      }
+    },
+    showDialog(callback) {
+      //$("#modaldialog_layer").modal("show");
+      if(callback) $(this.$refs.dialogForm.$el).on("shown.bs.modal",callback);
+      $(this.$refs.dialogForm.$el).modal("show");
+    },  
+    hideDialog() {
+      //$("#modaldialog_layer").modal("hide");
+      $(this.$refs.dialogForm.$el).modal("hide");
+    },
+    resetRecord() {
+      //reset to default data 
+      this.reset(defaultData,{action:"insert"});
+      //reset validator
+      this.v$.$reset();
+      //enable key field
+      this.disabledKeyField = false;
+    },
+    startInsertRecord() {
+      this.resetRecord();
+      this.showDialog(() => { this.$refs.branch.focus(); });
+    },
+    startSaveRecord() {
+      confirmSave(() => {
+        this.saveRecord(this.localData);
+      });
+    },
+    startUpdateRecord() {
+      confirmUpdate(() => { 
+        this.updateRecord(this.localData);
+      });
+    },
+    startDeleteRecord(dataKeys) {
+      confirmDelete(Object.values(dataKeys),() => {
+        this.deleteRecord(dataKeys);
+      });
+    },
+    saveRecord(dataRecord) {
+        let jsondata = {ajax: true};
+        let formdata = serializeParameters(jsondata,dataRecord);
+        startWaiting();
+        $.ajax({
+          url: getApiUrl()+APP_URL+"/insert",
+          data: formdata.jsondata,
+          headers : formdata.headers,
+          type: "POST",
+          dataType: "json",
+          contentType: DEFAULT_CONTENT_TYPE,
+          error : function(transport,status,errorThrown) {
+            console.error("error: status",status,"errorThrown",errorThrown);
+            submitFailure(transport,status,errorThrown);
+          },
+          success: (data) => {
+            stopWaiting();
+            console.log("success",data);
+            if(detectErrorResponse(data)) return;
+            successbox(() => {
+              //reset data for new record insert
+              this.resetRecord();
+              setTimeout(() => { this.$refs.branch.focus(); },100);
+              this.$emit('data-saved',dataRecord,data);
+            });
+          }
+      });
+    },
+    updateRecord(dataRecord) {
+        let jsondata = {ajax: true};
+        let formdata = serializeParameters(jsondata,dataRecord);
+        startWaiting();
+        $.ajax({
+          url: getApiUrl()+APP_URL+"/update",
+          data: formdata.jsondata,
+          headers : formdata.headers,
+          type: "POST",
+          dataType: "json",
+          contentType: DEFAULT_CONTENT_TYPE,
+          error : function(transport,status,errorThrown) {
+            console.error("error: status",status,"errorThrown",errorThrown);
+            submitFailure(transport,status,errorThrown);
+          },
+          success: (data) => {
+            stopWaiting();
+            console.log("success",data);
+            if(detectErrorResponse(data)) return;
+            successbox(() => {
+              this.hideDialog();
+              this.$emit('data-updated',dataRecord,data);
+            });
+          }
+      });
+    },
+    retrieveRecord(dataKeys) {
+      let jsondata = {ajax: true};
+      let formdata = serializeParameters(jsondata,dataKeys);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+APP_URL+"/retrieve",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("retrieveRecord: error: status",status,"errorThrown",errorThrown);
+          submitFailure(transport,status,errorThrown);
+        },
+        success: (data) => {
+          stopWaiting();
+          console.log("retrieveRecord: success",data);
+          if(data.body.dataset) {
+            this.reset(data.body.dataset,{action:"edit"});
+            this.v$.$reset();
+            this.disabledKeyField = true;
+            this.showDialog(() => { this.$refs.nameen.focus(); });
+          }
+        }
+      });	
+    },
+    deleteRecord(dataKeys) {
+      let jsondata = {ajax: true};
+      let formdata = serializeParameters(jsondata,dataKeys);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+APP_URL+"/remove",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("deleteRecord: error: status",status,"errorThrown",errorThrown);
+          submitFailure(transport,status,errorThrown);
+        },
+        success: (data) => {
+          stopWaiting();
+          console.log("deleteRecord: success",data);
+          if(detectErrorResponse(data)) return;
+          successbox(() => {
+            this.$emit('data-deleted',dataKeys,data);
+          });
+        }
+      });	
+    },
+  }
+};
+</script>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,163 @@
+<template>
+<div id="searchpanel" class="panel-body search-panel">
+    <div class="row row-height">
+      <div class="col-height col-md-3">
+            <label for="nameens">{{ labels.nameens_label }}</label>
+            <input type="text" v-model="localData.nameen" class="form-control input-md" maxlength="100" id="nameens" />
+        </div>
+        <div class="col-height col-md-3">
+            <label for="nameths">{{ labels.nameths_label }}</label>
+            <input type="text" v-model="localData.nameth" class="form-control input-md" maxlength="100" id="nameths" />
+        </div>
+        <div class="col-height col-md-2">
+            <label for="branchtypes">{{ labels.branchtypes_label }}</label>
+            <select ref="branchtype" id="branchtypes" v-model="localData.branchtype" class="form-control input-md">
+              <option value=""></option>
+              <option v-for="item in dataCategory.tkbranchtype" :key="item.id" :value="item.id">{{item.text}}</option>
+            </select>
+        </div>
+        <div class="col-height col-md">
+            <br/>
+            <button @click="searchClick" class="btn btn-dark btn-sm btn-ctrl"><i class="fa fa-search fa-btn-icon" aria-hidden="true"></i>{{ labels.search_button }}</button>
+            <button @click="resetClick" class="btn btn-dark btn-sm btn-ctrl"><i class="fa fa-refresh fa-btn-icon" aria-hidden="true"></i>{{ labels.reset_button }}</button>
+            <button @click="insertClick" class="btn btn-dark btn-sm btn-ctrl pull-right"><i class="fa fa-plus fa-btn-icon" aria-hidden="true"></i>{{ labels.insert_button }}</button>
+        </div>
+    </div>
+    <div id="listpanel" class="table-responsive fa-list-panel">
+        <DataTable ref="dataTable" :settings="tableSettings" :labels="labels" :dataset="dataset" @data-select="dataSelected" @data-sort="dataSorted" />
+        <DataPaging ref="dataPaging" :settings="pagingSettings" @page-select="pageSelected" />
+    </div>
+</div>
+</template>
+<script>
+import { ref } from 'vue';
+import $ from "jquery";
+import { DEFAULT_CONTENT_TYPE, getApiUrl }  from '@willsofts/will-app';
+import { startWaiting, stopWaiting, submitFailure, serializeParameters }  from '@willsofts/will-app'
+import { Paging } from "@willsofts/will-app";
+import { DataTable, DataPaging } from '@willsofts/will-control';
+
+const APP_URL = "/api/sfte019";
+const defaultData = {
+  nameen: "",
+  nameth: "",
+  branchtype: "",
+};
+
+const tableSettings = {
+    sequence: { label: "seqno_label" },
+    columns: [
+        {name: "branch", type: "STRING", sorter: "branch", label: "branch_headerlabel", css: "text-center" },
+        {name: "nameen", type: "STRING", sorter: "nameen", label: "nameen_headerlabel" },
+        {name: "nameth", type: "STRING", sorter: "nameth", label: "nameth_headerlabel" },
+        {name: "branchtypename", type: "STRING", sorter: "branchtype", label: "branchtype_headerlabel" }
+    ],        
+    actions: [
+        {type: "button", action: "edit"},
+        {type: "button", action: "delete"},
+    ],
+};
+
+export default {
+  components: {
+    DataTable, DataPaging
+  },
+  props: {
+    labels: Object,
+    formData: Object,
+    dataCategory: Object
+  },
+  emits: ["data-select","data-insert"],
+  setup(props) {
+    const localData = ref({ ...defaultData, ...props.formData });
+    let paging = new Paging();
+    let pagingSettings = paging.setting;
+    let filters = {};
+    const dataset = ref({});
+    return { localData, tableSettings, pagingSettings, paging, filters, dataset };
+  },
+  methods: {
+    reset(newData) {
+      if(newData) this.localData = {...newData};
+    },
+    getPagingOptions(settings) {
+      if(!settings) settings = this.pagingSettings;
+      return {page: settings.page, limit: settings.limit, rowsPerPage: settings.rowsPerPage, orderBy: settings.orderBy?settings.orderBy:"", orderDir: settings.orderDir?settings.orderDir:"" };
+    },
+    resetClick() {
+      this.localData = {...defaultData};
+      this.resetFilters();
+      this.$refs.dataTable.clear();
+      this.$refs.dataPaging.clear();
+      this.pagingSettings.rows = 0;
+    },
+    insertClick() {
+      this.$emit('data-insert',this.filters);
+    },
+    searchClick() {
+      this.filters = {...this.localData};
+      this.resetFilters();
+      this.search();
+    },
+    resetFilters() {
+      this.pagingSettings.page = 1;
+      this.pagingSettings.orderBy = "";
+      this.pagingSettings.orderDir = "";
+    },
+    search(ensurePaging=false) {
+      if(ensurePaging) {
+        if(this.pagingSettings.rows <= 1 && this.pagingSettings.page > 1) {
+          this.pagingSettings.page = this.pagingSettings.page - 1;
+        }
+      }
+      console.log("search: pagingSettings",this.pagingSettings);
+      let options = this.getPagingOptions();
+      this.collecting(options,this.filters);
+    },
+    collecting(options,criterias) {
+      console.log("collecting: options",options,", criteria",criterias);
+      let jsondata = Object.assign({ajax: true},options);
+      let formdata = serializeParameters(jsondata,criterias);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+APP_URL+"/collect",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("error: status",status,"errorThrown",errorThrown);
+          submitFailure(transport,status,errorThrown);
+        },
+        success: (data) => {
+          stopWaiting();
+          console.log("collecting: success",data);
+          if(data.body) {
+            this.$refs.dataTable.reset(data.body);
+            this.$refs.dataPaging.reset(data.body?.offsets);
+            this.pagingSettings.rows = data.body?.rows?.length;
+          }
+        }
+      });	
+    },
+    pageSelected(item) {
+      console.log("page selected:",item);
+      this.pagingSettings.page = item.page;
+      let options = this.getPagingOptions();
+      this.collecting(options,this.filters);
+    },
+    dataSelected(item,action) {
+      console.log("dataSelected",item,"action",action);
+      this.$emit('data-select', item, action);
+    },
+    dataSorted(sorter,direction) {
+      console.log("dataSorted",sorter,"direction",direction);
+      this.pagingSettings.orderBy = sorter;
+      this.pagingSettings.orderDir = direction;
+      let options = this.getPagingOptions();
+      this.collecting(options,this.filters);
+    },
+  }
+};
+</script>
